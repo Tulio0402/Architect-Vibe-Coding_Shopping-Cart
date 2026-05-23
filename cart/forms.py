b42622c0a7afa0f80b1509eeee1acb34c16909a1
Tulio <tuliosun402@gmail.com>
2026-05-24 03:12:20 +0800

    v1

---
+++ cart/forms.py
@@ -0,0 +1,7 @@
+from django import forms  # 修正這裡：刪除錯誤的 TrueForm 導入
+
+PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
+
+class CartAddProductForm(forms.Form):
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int, label='數量')
+    override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
